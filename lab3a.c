d884bdadad606711ec1f45e03453149c7ac6f2e2
Christopher Ngai <christopherngai@s-128-97-250-60.resnet.ucla.edu>
2018-03-02 18:30:00 -0800

    initial commit

---
+++ lab3a.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ext2_fs.h"
+
+/* get values for superblock summary */
+void get_sbs() {
+  return;
+}
+
+/* get values for group summary */
+void get_gs() {
+  return;
+}
+
+/* get values for free block entries */
+void get_fbe() {
+  return;
+}
+
+/* get values for free i-node entries */
+void get_fie() {
+  return;
+}
+
+/* get values for i-node summary */
+void get_is() {
+  return;
+}
+
+/* get values for directory entries */
+void get_de() {
+  return;
+}
+
+/* get values for indirect block references */
+void get_ibr() {
+  return;
+}
+
+int main(int argc, char * argv[]) {
+  get_sbs();
+  get_gs();
+  get_fbe();
+  get_fie();
+  get_is();
+  get_de();
+  get_ibr();
+  exit(0);
+}
